586f28a7817f21d071c3f304dde3f985f0eb1db5
shinekumar23 <shinekumar23@gmail.com>
2020-08-04 17:54:40 +0530

    Sorted in descending order

--- ArithmaticComputationSorting.sh
+++ ArithmaticComputationSorting.sh
@@ -3,15 +3,29 @@ read -p "Enter first number: " a
 read -p "Enter second number: " b
 read -p "Enter third number: " c
 declare -A compute
-compute[a+b*c]=[$(($a+$(($b*$c))))]
-compute[a*b+c]=[$(($(($a*$b))+$c))]
-compute[c+a/b]=[$(($c+$(($a/$b))))]
-compute[a%b+c]=[$(($(($a%$b))+$c))]
+compute[a+b*c]="$(($a+$(($b*$c))))"
+compute[a*b+c]="$(($(($a*$b))+$c))"
+compute[c+a/b]="$(($c+$(($a/$b))))"
+compute[a%b+c]="$(($(($a%$b))+$c))"
+
 
 for i in ${compute[@]}
 do
    	array[j++]=$i
 done
 
+for (( i=0; i<4; i++ ))
+do
+      for (( k=i+1; k<4; k++ ))
+      do
+               if [ $((array[$i])) -lt $((array[$k])) ]
+               then
+                     temp=$((array[$i]))
+                     array[$i]=$((array[$k]))
+                     array[$k]=$temp
+               fi
+      done
+done
+echo "The sorted array in Descending order: ${array[@]}"
 
 
